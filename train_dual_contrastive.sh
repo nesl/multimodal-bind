a02bd585136070f5d1db172a6a19645bf0837dbf
Tomoyoshi Kimura <tkimura4@illinois.edu>
2024-06-10 13:44:21 +0800

    minor

--- train_dual_contrastive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd train
-python3 main_dualcontrastive_1_train_contrastive.py --seed $1
-cd ../evaluation
-python3 main_fuse_sup_dual_contrastive.py --seed $1
-cd save_train_C/label_216/dual_contrastive/results
-mv test_accuracy.txt test_accuracy_${1}.txt
--- train_lowerbound.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd evaluation
-python3 main_fuse_sup_lower_bound.py --seed $1
-cd save_train_C/label_216/supfuse_lower_bound/results
-mv test_accuracy.txt test_accuracy_${1}.txt
